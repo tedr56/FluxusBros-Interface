72527ace66a5027402e3ac6fa91ec580cc4fa6e3
VJ T <ted.r@hotmail.fr>
2011-08-24 05:16:18 +0200

    Midi Global Variable added in MidiVars.py

---
+++ MidiVars.py
@@ -0,0 +1,10 @@
+                #Clock Raw Data
+        #clock tick     = 248(10) F8(16)
+        #clock start    = 250(10) FA(16)
+        #clock continue = 251(10) FB(16)
+        #clock stop     = 252(10) FC(16)
+        
+MIDI_CLOCK_TICK = 248
+MIDI_CLOCK_START = 250
+MIDI_CLOCK_CONTINUE = 251
+MIDI_CLOCK_STOP = 252
